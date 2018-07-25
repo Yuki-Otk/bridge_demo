5dca2a13c5f0f5d79a5931ea19382cb9d14e0598
YukiOtake <djotkyu@gmail.com>
2018-07-25 13:43:09 +0900

    add_橋を表示する(twoHandで)

---
+++ Assets/Script/showImage.cs
@@ -0,0 +1,33 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class showImage : MonoBehaviour , IInputClickHandler
+{
+    public GameObject image;
+    int count;
+    // Use this for initialization
+    void Start () {
+        count = 0;
+        image.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        if (count % 2 != 0)
+        {
+            image.SetActive(false);
+        }
+        else
+        {
+            image.SetActive(true);
+        }
+        count++;
+
+    }
+}
