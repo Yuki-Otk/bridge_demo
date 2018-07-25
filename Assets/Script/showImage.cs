455586fa4e0e59e0abc802fd75130d884078984e
YukiOtake <djotkyu@gmail.com>
2018-07-25 17:29:18 +0900

    add_音声での操作

--- Assets/Script/showImage.cs
+++ Assets/Script/showImage.cs
@@ -6,11 +6,13 @@ using UnityEngine;
 public class showImage : MonoBehaviour , IInputClickHandler
 {
     public GameObject image;
+    private Camera holoCamera;
     int count;
     // Use this for initialization
     void Start () {
+        holoCamera = Camera.main;
         count = 0;
-        image.SetActive(false);
+        //image.SetActive(false);
     }
 	
 	// Update is called once per frame
@@ -25,6 +27,13 @@ public class showImage : MonoBehaviour , IInputClickHandler
         }
         else
         {
+            // カメラの目の前 1.25 Unit（移動目標地点、1 Unit = 1 m くらいのはず？ ）の座標取得
+            Vector3 frontPosition = holoCamera.transform.position + holoCamera.transform.forward * 1.25f;
+            frontPosition.z = frontPosition.z + 1f;
+            //frontPosition.y = 0f;
+            image.transform.position = frontPosition;
+            image.transform.LookAt(holoCamera.transform.position);
+            image.transform.Rotate(new Vector3(0, 180, 0));
             image.SetActive(true);
         }
         count++;
---
+++ Assets/Script/showMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class showMenu : MonoBehaviour {
+    public GameObject planDesignButton;
+    public GameObject planDesign;
+    private Camera holoCamera;
+    // Use this for initialization
+    void Start () {
+        planDesignButton.SetActive(false);
+        planDesign.SetActive(false);
+        holoCamera = Camera.main;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void OnRecognizedMenu()
+    {
+        Debug.Log("menu");
+        // カメラの目の前 1.25 Unit（移動目標地点、1 Unit = 1 m くらいのはず？ ）の座標取得
+        Vector3 frontPosition = holoCamera.transform.position + holoCamera.transform.forward * 1.25f;
+        //frontPosition.y = 0f;
+        planDesignButton.transform.position = frontPosition;
+        planDesignButton.transform.LookAt(holoCamera.transform.position);
+        planDesignButton.transform.Rotate(new Vector3(0, 180, 0));
+        planDesignButton.SetActive(true);
+    }
+    public void OnRecognizedClear()
+    {
+        planDesignButton.SetActive(false);
+        planDesign.SetActive(false);
+    }
+}
